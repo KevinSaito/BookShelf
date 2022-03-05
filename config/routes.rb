3477ca4286b38a598c21f4c9385aca968ff66bc4
KevinSaito <u6cfdm3p@gmail.com>
2022-03-05 04:53:41 +0000

    routes.rb変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
+  root to:'public/homes#top'
+  
+  #顧客用
   devise_for :users, controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
-  root to:'homes#top'
-  get 'homes/about' => 'homes#about',as:'about'
+
+  #顧客側
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
