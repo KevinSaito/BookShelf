d522992e08eb3c1b7b6d3e138d57aea570feb2e9
KevinSaito <u6cfdm3p@gmail.com>
2022-03-07 06:11:52 +0000

    いいね一覧搭載

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -2,18 +2,20 @@ class Public::UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @favo_images = @user.favo_images
+    favorites = Favorite.where(user_id: current_user.id).pluck(:favo_image_id)
+    @favorite_list = FavoImage.find(favorites)
   end
 
   def edit
     @user = User.find(params[:id])
   end
-  
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end
-  
+
   private
   def user_params
     params.require(:user).permit(:name, :introduction, :profile_image)
