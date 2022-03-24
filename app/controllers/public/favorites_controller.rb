3786c4f0c25ad2e19adf59246c3b7fbc0a38fca5
KevinSaito <u6cfdm3p@gmail.com>
2022-03-24 10:46:20 +0000

    いいね非同期完了

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,15 +1,13 @@
 class Public::FavoritesController < ApplicationController
   def create
-    favo_image = FavoImage.find(params[:favo_image_id])
-    favorite = current_user.favorites.new(favo_image_id: favo_image.id)
+    @favo_image = FavoImage.find(params[:favo_image_id])
+    favorite = current_user.favorites.new(favo_image_id: @favo_image.id)
     favorite.save
-    redirect_to favo_image_path(favo_image.id)
   end
 
   def destroy
-    favo_image = FavoImage.find(params[:favo_image_id])
-    favorite = current_user.favorites.find_by(favo_image_id: favo_image.id)
+    @favo_image = FavoImage.find(params[:favo_image_id])
+    favorite = current_user.favorites.find_by(favo_image_id: @favo_image.id)
     favorite.destroy
-    redirect_to favo_image_path(favo_image.id)
   end
 end
